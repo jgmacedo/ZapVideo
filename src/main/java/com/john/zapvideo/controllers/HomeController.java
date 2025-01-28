a662be2b0c6d2be34cde451a507b3f91e609bbbb
João Gabriel Macedo <jgabreumacedo@gmail.com>
2025-01-28 13:26:26 -0300

    website working perfecly. Enhance front-end is next

--- src/main/java/com/john/zapvideo/controllers/HomeController.java
+++ src/main/java/com/john/zapvideo/controllers/HomeController.java
@@ -2,14 +2,21 @@ package com.john.zapvideo.controllers;
 
 import com.john.zapvideo.services.VideoService;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.*;
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.net.URLEncoder;
-import java.nio.file.Files;
-
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 @RequestMapping("/")
@@ -28,66 +35,38 @@ public class HomeController {
     @PostMapping("/process")
     public String processVideo(@RequestParam String url, Model model) {
         String id = null;
-        String filePath = null;
         try {
-            // Processa o vídeo e obtém o ID
             id = VideoService.processVideo(url);
-            // Obtém o caminho do arquivo usando o ID
-            filePath = VideoService.getFilePathById(id);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
-        // Se o caminho do arquivo existir, redireciona para a página de download
-        if (filePath != null) {
-            String encodedFilePath = null;
-            try {
-                encodedFilePath = URLEncoder.encode(filePath, "UTF-8");
-            } catch (UnsupportedEncodingException e) {
-                throw new RuntimeException(e);
-            }
-            return "redirect:/download?filePath=" + encodedFilePath;
+        if (id != null) {
+            return "redirect:/downloadPage?id=" + id;
         } else {
             model.addAttribute("error", "Video processing failed");
-            return "error"; // Exibe uma página de erro caso o processamento falhe
+            return "error"; // Display an error page if processing fails
         }
     }
 
     // Serve o arquivo para download
     @GetMapping("/download")
-    @ResponseBody
-    public void downloadFile(@RequestParam String filePath, HttpServletResponse response) throws IOException {
-        if (!isValidFilePath(filePath)) {
-            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-            return;
-        }
-
-        File file = new File(filePath);
-        if (!file.exists()) {
-            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-            return;
-        }
-
-        response.setContentType(Files.probeContentType(file.toPath()));
-        response.setHeader("Content-Disposition", "attachment; filename=\"" + file.getName() + "\"");
-        response.setContentLengthLong(file.length());
-
-        try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
-             BufferedOutputStream out = new BufferedOutputStream(response.getOutputStream())) {
-
-            byte[] buffer = new byte[8192];
-            int bytesRead;
-
-            while ((bytesRead = in.read(buffer)) != -1) {
-                out.write(buffer, 0, bytesRead);
+    public ResponseEntity<Resource> downloadFile(@RequestParam String filePath) {
+        Path path = Paths.get(filePath);
+        Resource resource;
+        try {
+            resource = new UrlResource(path.toUri());
+            if (!resource.exists() || !resource.isReadable()) {
+                throw new RuntimeException("File not found or not readable");
             }
-            out.flush();
-        } catch (IOException e) {
-            throw new RuntimeException("Erro ao ler o arquivo", e);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Error in file path", e);
         }
 
-        String videoId = extractVideoIdFromFilePath(filePath);
-        VideoService.deleteVideoById(videoId); // Deleta o vídeo após o download
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + path.getFileName().toString() + "\"")
+                .body(resource);
     }
 
     // Redireciona para a página de download
@@ -108,6 +87,27 @@ public class HomeController {
         }
 
         return "redirect:/download?filePath=" + encodedFilePath;
+    }
+
+    @GetMapping("/downloadPage")
+    public String downloadPage(@RequestParam String id, Model model) {
+        String filePath = VideoService.getFilePathById(id);
+
+        if (filePath == null) {
+            model.addAttribute("error", "Video not found");
+            return "error"; // Display an error page if the video is not found
+        }
+
+        String encodedFilePath = null;
+        try {
+            encodedFilePath = URLEncoder.encode(filePath, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(encodedFilePath);
+        System.out.println(id);
+
+        return "redirect:/download.html?filePath=" + encodedFilePath + "&id=" + id; // Redirect to the static HTML page with the filePath and video ID
     }
 
     private String extractVideoIdFromFilePath(String filePath) {
